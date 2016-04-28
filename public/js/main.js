bb7aa02b0360b544a7ee67067f7e57ad01589c4a
Alexey Ershov <alex4rom@gmail.com>
2016-04-28 05:11:30 +0300

    Clean up a bit

--- public/js/main.js
+++ public/js/main.js
@@ -5,6 +5,9 @@
   // clean up code
   // Use React?
 
+  // Add 'likes' and 'top' ?
+  // Add clicks counter ?
+
 function loadJSON(callback) {   
 
   var xobj = new XMLHttpRequest();
@@ -24,15 +27,16 @@ var words;
 var adj_len,
     noun_len;
 
-var ADJ,
-    NOUNS;
+var adj,
+    nouns;
+
+var loadedImgs = [];
 
 function init() {
  loadJSON(function(response) {
-  // Parse JSON string into object
     words = JSON.parse(response);
-    ADJ = words['adjectives'];
-    NOUNS = words['nouns'];
+    adj = words['adjectives'];
+    nouns = words['nouns'];
 
     adj_len = words['adjectives'].length;
     noun_len = words['nouns'].length;
@@ -41,90 +45,83 @@ function init() {
 
 init();
 
+var IMAGESNUM = 22;
 
-  var BGCHANGELIMIT = 11;
-  var IMAGESNUM = 22;
+var bgs = createBg('bg', IMAGESNUM, 'png');
+var bgs_len = bgs.length;
 
-  var bgs = createBg('bg', IMAGESNUM, 'png');
-  var bgs_len = bgs.length;
+var btnClicks = 0;
 
-  var btnClicks = 0;
+var doc = document,
+    body = doc.getElementsByTagName('body')[0],
+    content = doc.getElementById('content'),
+    generator = doc.getElementById('generator'),
+    preloadEl = doc.getElementById('preload');
 
-  var doc = document,
-      body = doc.getElementsByTagName('body')[0],
-      content = doc.getElementById('content'),
-      generator = doc.getElementById('generator'),
-      preloadEl = doc.getElementById('preload');
+// Functions
 
-  // Functions
+function generate() {
+  content.textContent = 'Лолита, ты - ' + ' ' +
+                        adj[randNumFromArray(adj_len)] + ' ' +
+                        adj[randNumFromArray(adj_len)] + ' ' +
+                        nouns[randNumFromArray(noun_len)];
+}
 
-  function generate(){
-    btnClicks += 1;
-    content.textContent = 'Лолита, ты - ' + ' ' +
-                          ADJ[randNumFromArray(adj_len)] + ' ' +
-                          ADJ[randNumFromArray(adj_len)] + ' ' +
-                          NOUNS[randNumFromArray(noun_len)];
-  }
+function createBg(name, num, ext) {
+  var arr = Array.apply(null, Array(num));
+  return arr.map(function (x, i) {
+    return name + (i + 1) + '.' + ext;
+  });
+}
 
-  function createBg(name, num, ext) {
-    var arr = Array.apply(null, Array(num));
-    return arr.map(function (x, i) {
-      return name + (i + 1) + '.' + ext;
+function loadImg(src, callback) {
+    var img = new Image();
+    img.onload = callback;
+    img.src = src;
+}
+
+function preloadImgs(imgs) {
+  for (i = 0; i < imgs.length; i++) {
+    var imgSrc = 'img/' + imgs[i];
+    loadImg(imgSrc, function() {
+      console.log('done ', imgSrc);
+      return loadedImgs.push(imgSrc);
     });
   }
+}
 
-    function loadSprite(src, callback) {
-        var sprite = new Image();
-        sprite.onload = callback;
-        sprite.src = src;
+
+preloadImgs(bgs);
+
+function prepareImages(img) {
+  var imgs = imgs ? imgs : [];
+  console.log('tick', imgs);
+  return imgs.push(img);
+}
+
+function startSlideShow() {
+  setInterval(function() {
+    console.log('tick');
+    if (loadedImgs.length === bgs.length) {
+      randBgImage();
     }
+  }, 10000)
+}
 
-    var loadedImgs = [];
-    function preloadImgs(imgs) {
-      for (i = 0; i < imgs.length; i++) {
-        var imgSrc = 'img/' + imgs[i];
-        loadSprite(imgSrc, function() {
-          console.log('done ', imgSrc);
-          return loadedImgs.push(imgSrc);
-        });
-      }
-    }
+startSlideShow();
 
+function randNumFromArray(len) {
+  return parseInt(Math.random() * len);
+}
 
-  preloadImgs(bgs);
+function randBgImage() {
+  var imageUrl = 'img/' + bgs[randNumFromArray(bgs_len)];
+  body.style.backgroundImage = 'url(' + imageUrl + ')';
+}
 
-  function prepareImages(img) {
-    var imgs = imgs ? imgs : [];
-    console.log('tick', imgs);
-    return imgs.push(img);
-  }
+// Event listeners
 
-  function startSlideShow() {
-    setInterval(function() {
-      console.log('tick');
-      if (loadedImgs.length === bgs.length) {
-        randBgImage();
-      }
-    }, 10000)
-  }
+generator.addEventListener("click", generate, false);
 
-  startSlideShow();
-
-  function randNumFromArray(len) {
-    return parseInt(Math.random() * len);
-  }
-
-  function randBgImage() {
-    var imageUrl = 'img/' + bgs[randNumFromArray(bgs_len)];
-    body.style.backgroundImage = 'url(' + imageUrl + ')';
-  }
-
-  // Event listeners
-
-  generator.addEventListener("click", generate, false);
-
-  // Start
-  // preloadBg();
-  // randBgImage();
 
 // })()
